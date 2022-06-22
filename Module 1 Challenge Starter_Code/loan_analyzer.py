08f85a96560cd6a482b88eddbfbd8452d2faab30
Tash <taofik.sulaiman2@gmail.com>
2022-06-22 00:46:51 -0500

    Finished Part 4: Perform Financial Calculations

--- Module 1 Challenge Starter_Code/loan_analyzer.py
+++ Module 1 Challenge Starter_Code/loan_analyzer.py
@@ -81,8 +81,8 @@ print(f"The remaining months on this loan is: {remaining_months}")
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 
 # YOUR CODE HERE!
-discount_rate = 0.20
-fair_value = future_value/((1 + discount_rate/12)**remaining_months)
+annual_discount_rate = 0.20
+fair_value = future_value/((1 + annual_discount_rate/12)**remaining_months)
 print(f"The fair value of this loan is: ${fair_value:.2f}")
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
@@ -119,12 +119,19 @@ new_loan = {
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
-
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    fair_value = future_value/((1 + annual_discount_rate/12)**remaining_months)
+    return fair_value
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+annual_discount_rate = 0.20
+present_value = calculate_present_value(
+    new_loan["future_value"],
+    new_loan["remaining_months"],
+    annual_discount_rate)
+print(f"The present value of the new loan is: ${present_value:.2f}")
 
 
 """Part 4: Conditionally filter lists of loans.
